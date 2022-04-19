fd764b883d7a64a66235a3f368ff5c2e68f750f3
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2022-04-19 16:30:27 +0900

    빈 화면 배포 문제 해결

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import Detail from "./routes/Detail";
 function App() {
   return  (
   <Router>
-    <Routes basename={process.env.PUBLIC_URL}>
+    <Routes>
       <Route path="/movie/:id" element={<Detail />}></Route>
-      <Route path="/" element={<Home />}></Route>
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
     </Routes>
   </Router>
   );
